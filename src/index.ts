3ae46db9ef7ae74c36c5376fa6b58974eedfd957
BrillicaTechnologies <ops@brillicatech.com>
2025-09-27 12:03:08 +0530

    app import removed

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
-import app from "./app";
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
 app.listen(3000, () => {
   console.log("Server started on port 3000");
--- tests/app.test.js
+++ tests/app.test.js
@@ -1,3 +1,4 @@
+"use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -7,11 +8,15 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-import request from "supertest";
-import app from "../src/app";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const supertest_1 = __importDefault(require("supertest"));
+const app_1 = __importDefault(require("../src/app"));
 describe("GET /", () => {
     it("should return Hello World", () => __awaiter(void 0, void 0, void 0, function* () {
-        const res = yield request(app).get("/");
+        const res = yield (0, supertest_1.default)(app_1.default).get("/");
         expect(res.status).toBe(200);
         expect(res.text).toBe("Hello World");
     }));
