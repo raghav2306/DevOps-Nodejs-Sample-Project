103d2d899b308a6defb182da178fcb3251bf5d58
BrillicaTechnologies <ops@brillicatech.com>
2025-09-29 18:21:34 +0530

    index.ts updated

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello Raghav");
+  res.send("Hello Tushar");
 });
 
 app.listen(3000, () => {
